9c8a877d3603808250b33525c50b6519f332e745
DavidCou <93839089+DavidCou@users.noreply.github.com>
2024-12-20 21:17:48 -0500

    Added frame rate smoothing and a new background color

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,4 +1,4 @@
-import sys # Needed so we can quit the game
+import sys # Needed to quit the game
 import pygame # Needed to run the game
 
 class AlienInvaion:
@@ -7,10 +7,15 @@ class AlienInvaion:
     def __init__(self):
         """Initialize the game and create game resources"""
         pygame.init()
+        # Creates a clock instance that is used to ensure a steady frame rate 
+        self.clock = pygame.time.Clock()
         
         # Sets the game window size and caption 
         self.screen = pygame.display.set_mode((900,700))
         pygame.display.set_caption("Alien Invasion")
+
+        # Set the background color.
+        self.bg_color = (230, 230, 230)
 
     def run_game(self):
         """Start the main loop for the game."""
@@ -21,9 +26,15 @@ class AlienInvaion:
                 if event.type == pygame.QUIT:
                     sys.exit()
             
+            # Redraw the screen during each pass through the loop.
+            self.screen.fill(self.bg_color)
+
             # Make the most recently drawn screen visable.
             pygame.display.flip()
-
+            # Sets the desired frame rate, 
+            # should work well on most systems but may needed to be left out 
+            # completely on select systems 
+            self.clock.tick(60)
 if __name__ == '__main__':
     # Make a game instance, and run the game.
     ai = AlienInvaion()
