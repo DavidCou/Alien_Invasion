12f07df6e997e0fc937d053e70712f03d7cee199
DavidCou <93839089+DavidCou@users.noreply.github.com>
2024-12-28 17:30:01 -0500

    Added the ability for the player to fire bullets from the ship using the space key

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,7 +1,8 @@
 import sys # Needed to quit the game.
-import pygame # Needed to run the game.
+import pygame
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 class AlienInvaion:
     """Main class to manage game game assets and behavior."""
@@ -17,22 +18,38 @@ class AlienInvaion:
         self.screen = pygame.display.set_mode((self.settings.screen_width, 
                                                self.settings.screen_height))
         # To be used in a future update to allow the game to be played in fullscreen
+        # Note: Pygame does not have a default method of exiting the game 
+        # while in full screen, so ensure there is at least one working method 
+        # of exiting the game before uncommenting the code below 
         # self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
         # self.settings.screen_width = self.screen.get_rect().width
         # self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
 
     def run_game(self):
         """Start the main loop for the game."""
         while True:
             self._check_events()
             self.ship.update()
+            self.bullets.update()
+
+            # Get rid of bullets that have disappeared.
+            # Note: A copy of the bullet group is created in the for loop 
+            # so that the bullets can be removed from the bullet group while 
+            # the loop is running otherwise the the bullets will not be able 
+            # to be removed using this method.
+            for bullet in self.bullets.copy():
+                if bullet.rect.bottom <= 0:
+                    self.bullets.remove(bullet)   
+
             self._update_screen()
-            # Sets the desired frame rate, 
-            # should work well on most systems but may needed to be removed 
-            # on select systems for the frame rate to run smoothly. 
+            
+            # Set the desired frame rate, 
+            # should work well on most systems but may need to be removed 
+            # on select systems to improve game performance. 
             self.clock.tick(60)
 
     def _check_events(self):
@@ -55,6 +72,8 @@ class AlienInvaion:
             self.ship.moving_left = True
         elif event.key == pygame.K_q:
             sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
     
     def _check_keyup_events(self, event):
         """Respond to key releases."""
@@ -63,10 +82,17 @@ class AlienInvaion:
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
 
+    def _fire_bullet(self):
+        """Create a new bullet and add it to the bullets group."""
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
+
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
         # Redraw the screen during each pass through the loop.
         self.screen.fill(self.settings.bg_color)
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
         self.ship.blitme()
 
         # Make the most recently drawn screen visable.
