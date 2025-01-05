41a52a4250bc17368378dc6900f57ed84839c71a
DavidCou <93839089+DavidCou@users.noreply.github.com>
2025-01-05 16:02:53 -0500

    Added the alien fleet in

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
+from alien import Alien
 
 class AlienInvaion:
     """Main class to manage game game assets and behavior."""
@@ -10,6 +11,7 @@ class AlienInvaion:
     def __init__(self):
         """Initialize the game and create game resources"""
         pygame.init()
+        
         # Creates a clock instance that is used to ensure a steady frame rate. 
         self.clock = pygame.time.Clock()
         self.settings = Settings()
@@ -17,6 +19,7 @@ class AlienInvaion:
         # Sets the game window size and caption 
         self.screen = pygame.display.set_mode((self.settings.screen_width, 
                                                self.settings.screen_height))
+        
         # To be used in a future update to allow the game to be played in fullscreen
         # Note: Pygame does not have a default method of exiting the game 
         # while in full screen, so ensure there is at least one working method 
@@ -24,10 +27,14 @@ class AlienInvaion:
         # self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
         # self.settings.screen_width = self.screen.get_rect().width
         # self.settings.screen_height = self.screen.get_rect().height
+        
         pygame.display.set_caption("Alien Invasion")
-
+        
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
+        self.aliens = pygame.sprite.Group()
+
+        self._create_fleet()
 
     def run_game(self):
         """Start the main loop for the game."""
@@ -91,6 +98,32 @@ class AlienInvaion:
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet) 
 
+    def _create_fleet(self):
+        """Create the fleet of aliens."""
+        # Create an alien and keep adding aliens until there's no room left.
+        # Spacing between aliens is one alien width and one alien height, 
+        # ensuring the screen does not get over crowded with aliens.
+        alien = Alien(self)
+        alien_width, alien_height = alien.rect.size
+        current_x, current_y = alien_width, alien_height
+
+        while current_y < (self.settings.screen_height - 3 * alien_height):
+            while current_x < (self.settings.screen_width - 2 * alien_width):
+                self._create_alien(current_x, current_y)
+                current_x += 2 * alien_width
+            
+            # Finished a row; reset x value, and increment y value.
+            current_x = alien_width
+            current_y += 2 * alien_height
+        
+    def _create_alien(self, x_position, y_position):
+        """Create an alien and place it in the fleet."""
+        new_alien = Alien(self)
+        new_alien.x = x_position
+        new_alien.rect.x = x_position
+        new_alien.rect.y = y_position
+        self.aliens.add(new_alien)
+
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
         # Redraw the screen during each pass through the loop.
@@ -98,6 +131,7 @@ class AlienInvaion:
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
         self.ship.blitme()
+        self.aliens.draw(self.screen)
 
         # Make the most recently drawn screen visable.
         pygame.display.flip()
