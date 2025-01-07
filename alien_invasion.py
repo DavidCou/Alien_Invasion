47926628bfff9b4cad8b3f55493a1a9cf1500bf5
DavidCou <93839089+DavidCou@users.noreply.github.com>
2025-01-07 17:12:15 -0500

    The alien fleet now repopulates once the player has destroyed all of the aliens on screen, the game now stops once the player has run out of lives and the player is reset when they lose a life (either by colliding with an alien and/or an alien reaching the bottom of the screen)

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,10 @@
 import sys # Needed to quit the game.
+from time import sleep
+
 import pygame
+
 from settings import Settings
+from game_stats import GameStats
 from ship import Ship
 from bullet import Bullet
 from alien import Alien
@@ -19,7 +23,7 @@ class AlienInvaion:
         # Sets the game window size and caption 
         self.screen = pygame.display.set_mode((self.settings.screen_width, 
                                                self.settings.screen_height))
-        
+        pygame.display.set_caption("Alien Invasion")
         # To be used in a future update to allow the game to be played in fullscreen
         # Note: Pygame does not have a default method of exiting the game 
         # while in full screen, so ensure there is at least one working method 
@@ -28,7 +32,8 @@ class AlienInvaion:
         # self.settings.screen_width = self.screen.get_rect().width
         # self.settings.screen_height = self.screen.get_rect().height
         
-        pygame.display.set_caption("Alien Invasion")
+        # Create an instance to store game statistics.
+        self.stats = GameStats(self)
         
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
@@ -36,13 +41,19 @@ class AlienInvaion:
 
         self._create_fleet()
 
+        # Start Alien Invasion in an active state.
+        self.game_active = True
+
     def run_game(self):
         """Start the main loop for the game."""
         while True:
             self._check_events()
-            self.ship.update()
-            self._update_bullets()
-            self._update_aliens()
+            
+            if self.game_active:
+                self.ship.update()
+                self._update_bullets()
+                self._update_aliens()
+            
             self._update_screen()
             # Limits the frame rate to 60 fps.
             # Should work well on most systems but may need to be removed 
@@ -99,15 +110,57 @@ class AlienInvaion:
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
         
-        # Check for any bullets that have hit aliens.
-        #   If so, get rid of the bullet and the alien.
+        self._check_bullet_alien_collisions()
+         
+    def _check_bullet_alien_collisions(self):
+        """Respond to bullet-alien collisions."""
+        # Remove any bullets and aliens that have collided.
         collisions = pygame.sprite.groupcollide(
-            self.bullets, self.aliens, True, True)
+                self.bullets, self.aliens, True, True)
+        
+        if not self.aliens:
+           # Destroy existing bullets and create new fleet.
+           self.bullets.empty()
+           self._create_fleet()
 
     def _update_aliens(self):
         """Check if the fleet is at an edge, then update positions."""
         self._check_fleet_edges()
-        self.aliens.update() 
+        self.aliens.update()
+
+        # Look for alien-ship collisions.
+        if pygame.sprite.spritecollideany(self.ship, self.aliens):
+            self._ship_hit()
+        
+        # Look for aliens hitting the bottom of the screen.
+        self._check_aliens_bottom()
+
+    def _check_aliens_bottom(self):
+        """Check if any aliens have reached the bottom of the screen."""
+        for alien in self.aliens.sprites():
+            if alien.rect.bottom >= self.settings.screen_height:
+                # Treat this the same as if the ship got hit.
+                self._ship_hit()
+                break
+
+    def _ship_hit(self):
+        """Respond to the ship being hit by an alien."""
+        if self.stats.ships_left > 0:
+            # Decrement ships_left.
+            self.stats.ships_left -= 1
+
+            # Get rid of any remaining bullets and aliens.
+            self.bullets.empty()
+            self.aliens.empty()
+
+            # Create a new fleet and center the ship.
+            self._create_fleet()
+            self.ship.center_ship()
+
+            # Pause.
+            sleep(0.5)
+        else:
+            self.game_active = False
 
     def _create_fleet(self):
         """Create the fleet of aliens."""
